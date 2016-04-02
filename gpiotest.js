94682606ea5fa237bd4ddf59ce5ebbd36edd5a01
Lukas Ruge <lukeslog@googlemail.com>
2016-04-02 21:54:36 +0200

    first commit

---
+++ gpiotest.js
@@ -0,0 +1,103 @@
+var exec = require('exec');
+var dgram = require('dgram');
+var os = require('os');
+
+var before = true;
+var networkinterfaces = [];
+
+exec('raspi-gpio set 26 pu', function(a, b, c) {
+  setInterval(function() {
+    exec('raspi-gpio get 26', function(error, stdout, stderr) {
+      console.log(stdout);
+      if (stdout.search('level=1') > -1) {
+        leds(false);
+      } else {
+        leds(true);
+      }
+    });
+
+  }, 500);
+});
+
+
+function leds(on) {
+  console.log("-->" + on)
+  if (before == on) {
+    return;
+  }
+  before = on;
+  var dgramClient = dgram.createSocket('udp6');
+  var buf = new Buffer(630);
+  for (var i = 0; i < buf.length; i++) {
+    if (!on) {
+      buf[i] = 180;
+    } else {
+      buf[i] = 0;
+    }
+  }
+  console.log(buf);
+
+  sendLightCommand(dgramClient, buf, 0);
+  sendUdpMulticast(on);
+}
+
+function sendLightCommand(dgramClient, buf, counter) {
+  if (counter > 3) {
+    dgramClient.close();
+    return;
+  }
+  dgramClient.send(buf, 0, buf.length, 2812, '2a01:170:1112:0:bad8:12ff:fe66:fa1', function(err, bytes) {
+    if (err) {
+      throw err;
+    }
+    // Reuse the message buffer,
+    // or close client
+    sendLightCommand(dgramClient, buf, counter + 1);
+  });
+}
+
+function sendUdpMulticast(on) {
+  var interfaces = os.networkInterfaces();
+  networkinterfaces = [];
+  Object.keys(interfaces).forEach(function(ifname) {
+    console.log(ifname);
+    networkinterfaces.push(ifname);
+  });
+  buzz(on, 0);
+}
+
+function buzz(on, i) {
+  console.log("INTERAFACES:" + networkinterfaces.length);
+  var dgramServer = dgram.createSocket('udp6');
+  var message = createMessage(on);
+  if (i < networkinterfaces.length) {
+    sendMessage(dgramServer, message, networkinterfaces[i], function() {
+      buzz(on, i + 1);
+    });
+  } else {
+    dgramServer.close();
+  }
+}
+
+function sendMessage(dgramServer, message, networkinterface, callback) {
+  dgramServer.send(message, 0, message.length, 6006, 'FF02::6006%' + networkinterface, function(err, bytes) {
+    if (err) {
+      console.log("err when sending send UDP Message");
+    }
+    callback();
+    console.log("server close");
+  });
+}
+
+function createMessage(on) {
+  var date = new Date();
+  var dateString = date.getTime();
+  var event = {
+    eventtime: dateString,
+    kuelschranktueroffen: on
+  };
+  var message = new Buffer(JSON.stringify(event));
+  //var message = new Buffer("door");
+  console.log(message);
+  return message;
+}
